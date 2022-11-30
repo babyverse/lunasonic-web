286ab58821b8ef892365428065af5ccacba14b81
Vinko Bradvica <vinko.bradvica@gmail.com>
2022-11-30 22:23:58 +0100

    Add container

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,16 +2,19 @@ import React from 'react'
 
 import Layout from '../components/layout'
 import Landing from '../components/landing'
+import Container from '../components/container'
 
 class RootIndex extends React.Component {
   render() {
     return (
       <Layout location={this.props.location}>
-        <Landing
-          image={"pexels-transtudios-photography-&-video-10606514.jpg"}
-          title="Snuggly App"
-          content="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-        />
+        <Container>
+          <Landing
+            image={"pexels-transtudios-photography-&-video-10606514.jpg"}
+            title="Snuggly App"
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+          />
+        </Container>
       </Layout>
     )
   }
