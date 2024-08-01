6771156c7369d230fc838990eed346ca5bdcedb9
Vinko Bradvica <vinko.bradvica@gmail.com>
2024-08-01 17:09:26 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,8 +4,8 @@ require("dotenv").config({
 
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Contentful Starter",
-    description: "Official Contentful Gatsby Starter",
+    title: "Lunasonic | Snuggly App",
+    description: "Official website of Snuggly App",
   },
   plugins: [
     {
